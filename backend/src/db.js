b79f76d493cf85b53fe4986df4ce0972c5c9221b
Anders Nylund <anders.nylund.an@gmail.fi>
2018-11-24 13:27:56 +0200

    Module 3

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+// This file connects to the prosma DB and gices us the abiliity to query it with JS
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+// Here db calls could be inserted e.g. from another REST API
+
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  items: forwardTo('db')
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
