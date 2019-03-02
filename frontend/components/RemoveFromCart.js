e3ce52998957e60b875522f931858d7791b043be
Anders Nylund <anders.nylund.an@gmail.com>
2019-03-02 09:31:58 +0200

    Complete video 43

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -23,8 +23,31 @@ const BigButton = styled.button`
   }
 `;
 
+// This gets called as soon as we get a response back from the server
+// after a mutation has been performed
+const update = (cache, payload) => {
+  // first read the cache
+  const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+  // remove that item from the cart
+  const cartItemId = payload.data.removeFromCart.id;
+  data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+  // write it back to the cache
+  cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+};
+
 const RemoveFromCart = ({ id }) => (
-  <Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{ id }}>
+  <Mutation
+    mutation={REMOVE_FROM_CART_MUTATION}
+    variables={{ id }}
+    update={update}
+    optimisticResponse={{
+      __typename: 'Mutation',
+      removeFromCart: {
+        __typename: 'CartItem',
+        id,
+      },
+    }}
+  >
     {(removeMutation, { loading, error }) => (
       <BigButton
         disabled={loading}
