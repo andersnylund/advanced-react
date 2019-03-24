330b81937583b7c2b9f883548b59e9c64fee707d
Anders Nylund <anders.nylund.an@gmail.com>
2019-03-24 19:48:23 +0200

    Complete video 56

---
+++ frontend/__tests__/mocking.test.js
@@ -0,0 +1,35 @@
+function Person(name, foods) {
+  this.name = name;
+  this.foods = foods;
+}
+
+Person.prototype.fetchFavFoods = function() {
+  return new Promise((resolve, reject) => {
+    // simulating an API
+    setTimeout(() => resolve(this.foods), 2000);
+  });
+};
+
+describe('mocking learning', () => {
+  it('mocks a reg function', () => {
+    const fetchDogs = jest.fn();
+    fetchDogs('snickers');
+    expect(fetchDogs).toHaveBeenCalled();
+    expect(fetchDogs).toHaveBeenCalledWith('snickers');
+    fetchDogs('hugo');
+    expect(fetchDogs).toBeCalledTimes(2);
+  });
+
+  it('can create a person', () => {
+    const me = new Person('wes', ['pizza', 'burgs']);
+    expect(me.name).toBe('wes');
+  });
+
+  it('can fetch foods', async () => {
+    const me = new Person('wes', ['pizza', 'burgs']);
+    // mock the favFoods function
+    me.fetchFavFoods = jest.fn().mockResolvedValue(['pizza', 'ramen']);
+    const favFoods = await me.fetchFavFoods();
+    expect(favFoods).toContain('pizza');
+  });
+});
