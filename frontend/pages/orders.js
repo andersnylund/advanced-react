f650fea4400b1004e55fc648eb48c4ea4244cf29
Anders Nylund <anders.nylund.an@gmail.com>
2019-03-24 18:09:46 +0200

    Complete video 53

---
+++ frontend/pages/orders.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PleaseSignin from '../components/PleaseSignin';
+import OrderList from '../components/OrderList';
+
+const OrdersPage = () => (
+  <div>
+    <PleaseSignin>
+      <OrderList />
+    </PleaseSignin>
+  </div>
+);
+
+export default OrdersPage;
